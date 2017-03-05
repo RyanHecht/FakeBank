8989c3a1769019f14064e6d6393cd7746fdf379e
Ryan Hecht <ryanhecht.tse@gmail.com>
2017-03-05 13:17:27 -0500

    began route scaffolding

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
-
+var accountRoutes = require('./routes/accounts');
+var userRoutes = require('./routes/users');
+var transactionRoutes = require('./routes/transactions');
 var app = express();
 
 var port = 8080;
@@ -7,6 +9,11 @@ var port = 8080;
 app.get('/', function(req, res, next) {
   res.send("Welcome to FakeBank Inc.");
 });
+
+app.use("/users", userRoutes);
+app.use("/accounts", accountRoutes);
+app.use("/transactions", transactionRoutes);
+
 
 app.listen(port, function() {
   console.log("Server running on port " + port)
