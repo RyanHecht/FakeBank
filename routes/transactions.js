199880ee9d72ed255a893ad2c2f9768579782e6e
Ryan Hecht <ryanhecht.tse@gmail.com>
2017-03-05 14:02:04 -0500

    transaction route scaffold

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// will get all transactions
 router.get('/', function(req, res) {
   res.send("Transactions!!");
+});
+
+router.get('/:id', function(req, res) {
+  res.send("will get transaction by id");
+});
+
+router.post('/:sender/:receiver/:amount/:desc', function(req, res) {
+  res.send("will issue new transaction");
 });
 
 module.exports = router;
